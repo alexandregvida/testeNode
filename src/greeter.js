f704db6475f518af04d52efbb1f555ce7ac3cb4f
Alexandre G Vida <alexandregvida@gmail.com>
2018-03-18 17:16:18 -0300

    Calculadora

--- src/greeter.js
+++ src/greeter.js
@@ -1,3 +1,21 @@
 module.exports = function greet() {
     return "Hello!";
+
+
+var args = process.argv[2].split(' ')
+var num1 = args[0]
+var operator = args[1]
+var num2 = args[2]
+
+if (operator === '+') {
+    console.log(Number(num1) + Number(num2));
+} else if (operator === '-') {
+    console.log(Number(num1) - Number(num2));
+} else if (operator === '*') {
+    console.log(Number(num1) * Number(num2));
+} else if (operator === '/') {
+    console.log(Number(num1) / Number(num2));
+}
+
+
 }
