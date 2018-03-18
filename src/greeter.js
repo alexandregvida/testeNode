c4f50c8e2578855cf49a9ef786d0f7e23d2082f0
Alexandre G Vida <alexandregvida@gmail.com>
2018-03-18 20:39:28 -0300

    Add teste de Retorno

--- src/greeter.js
+++ src/greeter.js
@@ -1,21 +1,4 @@
 module.exports = function greet() {
-    return "Hello!";
-
-
-var args = process.argv[2].split(' ')
-var num1 = '1'
-var operator = '+'
-var num2 = '2'
-
-if (operator === '+') {
-    console.log(Number(num1) + Number(num2));
-} else if (operator === '-') {
-    console.log(Number(num1) - Number(num2));
-} else if (operator === '*') {
-    console.log(Number(num1) * Number(num2));
-} else if (operator === '/') {
-    console.log(Number(num1) / Number(num2));
-}
-
+    return 1+3;
 
 }
