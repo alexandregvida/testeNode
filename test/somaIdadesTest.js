806ce95aa9a9e7ef867fb97ce49bf2623e822892
Alexandre G Vida <alexandregvida@gmail.com>
2018-03-18 21:06:13 -0300

    Teste SomaIdades

--- test/somaIdadesTest.js
+++ test/somaIdadesTest.js
@@ -1,7 +1,7 @@
 var assert = require('assert');
-var greeter = require('../src/greeter.js');
+var greeter = require('../src/somaIdades.js');
 
-describe('Soma Mensagens', function() {
+describe('Soma Idades', function() {
   describe('Assert Message', function() {
     it('Soma Idades', function() {
       assert.equal(somaIdades(), 55);
